f6d7e2f50e68018f0cadaa9daf741c1b81df8ba1
Sajjad Salaria <imsafay@gmail.com>
2024-05-26 09:18:10 +0530

    ⚡ Creates Token entity and repository

---
+++ SurvivorsLitClub/src/main/java/com/xoraus/SurvivorsLitClub/user/Token.java
@@ -0,0 +1,29 @@
+package com.xoraus.SurvivorsLitClub.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Token {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String token;
+    private LocalDateTime createdAt;
+    private LocalDateTime expiresAt;
+    private LocalDateTime validatedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "userId", nullable = false)
+    private User user;
+}
