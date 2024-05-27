b8009e4b21bdf6bad1b75fbc41b8aa545ed0638b
Sajjad Salaria <imsafay@gmail.com>
2024-05-27 11:17:35 +0530

    ⚡ Implements Exception Handling

---
+++ SurvivorsLitClub/src/main/java/com/xoraus/SurvivorsLitClub/exception/ActivationTokenException.java
@@ -0,0 +1,7 @@
+package com.xoraus.SurvivorsLitClub.exception;
+
+public class ActivationTokenException extends RuntimeException {
+    public ActivationTokenException(String message) {
+        super(message);
+    }
+}
