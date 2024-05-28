aca145e09046f6fcd791872faa681ca3c6aaf018
Sajjad Salaria <imsafay@gmail.com>
2024-05-28 07:36:15 +0530

    ⚡ Creates Book Entity

---
+++ SurvivorsLitClub/src/main/java/com/xoraus/SurvivorsLitClub/book/Book.java
@@ -0,0 +1,40 @@
+package com.xoraus.SurvivorsLitClub.book;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class Book {
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    private String title;
+    private String authorName;
+    private String isbn;
+    private String synopsis;
+    private String bookCover;
+    private boolean archived;
+    private boolean shareable;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdDate;
+    @LastModifiedBy
+    @Column(insertable = false)
+    private LocalDateTime lastModifiedDate;
+    @CreatedBy
+    @Column(nullable = false, updatable = false)
+    private Integer createdBy;
+    @LastModifiedBy
+    @Column(insertable = false)
+    private Integer lastModifiedBy;
+}
