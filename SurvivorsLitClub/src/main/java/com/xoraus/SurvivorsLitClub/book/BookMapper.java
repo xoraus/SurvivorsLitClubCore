a2b0eab75592d33280a4f916dd06d5f67a24886c
Sajjad Salaria <imsafay@gmail.com>
2024-05-28 09:46:16 +0530

    ⚡ Implements save book method

---
+++ SurvivorsLitClub/src/main/java/com/xoraus/SurvivorsLitClub/book/BookMapper.java
@@ -0,0 +1,18 @@
+package com.xoraus.SurvivorsLitClub.book;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookMapper {
+    public Book toBook(BookRequest request) {
+        return Book.builder()
+                .id(request.id())
+                .title(request.title())
+                .isbn(request.isbn())
+                .authorName(request.authorName())
+                .synopsis(request.synopsis())
+                .archived(false)
+                .shareable(request.shareable())
+                .build();
+    }
+}
---
+++ SurvivorsLitClub/src/main/java/com/xoraus/SurvivorsLitClub/book/BookRepository.java
@@ -0,0 +1,10 @@
+package com.xoraus.SurvivorsLitClub.book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+
+public interface BookRepository extends JpaRepository<Book, Integer>, JpaSpecificationExecutor<Book> {
+
+}
